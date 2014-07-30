9903879ab6c45fad39d6710940e2c5abbe9cfd7f
root <root@debian.ispmgr5.template>
2014-07-30 05:38:55 -0400

    navigate links

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,6 @@ require(['app', 'router', 'models/sessionModel'], function(app, Router, Session)
 	app.router = new Router();
 	app.session = new Session();
 	app.session.checkAuth(function(){
-console.log(app.session.get('is_auth'));
 		Backbone.history.start({'pushState': true});
 	});
 });
